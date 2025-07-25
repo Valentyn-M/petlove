ca62f1a45a7f665ae796621a8cc07c9a72740814
Valentyn Mostovyi <mostovoy.v@gmail.com>
2025-07-25 15:46:00 +0200

    feat: add UserBlock component

---
+++ src/components/UserBlock/UserBlock.tsx
@@ -0,0 +1,9 @@
+import s from './UserBlock.module.scss';
+
+export interface UserBlockProps {}
+
+const UserBlock = ({}: UserBlockProps) => {
+  return <div></div>;
+};
+
+export default UserBlock;
--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -4,6 +4,7 @@ import { svgIcon } from '@/components/App';
 import { useModal } from '@/hooks/useModal';
 import Modal from '@/components/Modal/Modal';
 import ModalChildEditUser from '@/components/ModalChildEditUser/ModalChildEditUser';
+import UserBlock from '@/components/UserBlock/UserBlock';
 
 export interface UserCardProps {}
 
@@ -21,6 +22,8 @@ const UserCard = ({}: UserCardProps) => {
         </div>
         <ButtonFunction iconName="edit" className={s.btnEdit} onClick={() => openModal('edit-user')} />
       </div>
+
+      <UserBlock />
 
       {/* Modals */}
       {isModalOpen('edit-user') && (
