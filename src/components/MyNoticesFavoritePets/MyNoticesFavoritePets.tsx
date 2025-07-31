975d8867ef9282da78f4c245715dd8b042f94fb4
Valentyn Mostovyi <mostovoy.v@gmail.com>
2025-07-31 13:18:58 +0200

    feat: add MyNoticesFavoritePets component

--- src/components/MyNoticesFavoritePets/MyNoticesFavoritePets.tsx
+++ src/components/MyNoticesFavoritePets/MyNoticesFavoritePets.tsx
@@ -1,9 +1,34 @@
+import clsx from 'clsx';
 import s from './MyNoticesFavoritePets.module.scss';
+import { useAppSelector } from '@/store/hooks';
+import { selectAuthUserPetsNoticesFavorites } from '@/store/auth/selectors';
+import NoticesItem from '@/components/NoticesItem/NoticesItem';
 
 export interface MyNoticesFavoritePetsProps {}
 
 const MyNoticesFavoritePets = ({}: MyNoticesFavoritePetsProps) => {
-  return <div>MyNoticesFavoritePets</div>;
+  const favoritePets = useAppSelector(selectAuthUserPetsNoticesFavorites);
+
+  return (
+    <div className={s.myNoticesFavoritePets}>
+      {favoritePets.length > 0 ? (
+        <ul className={s.list}>
+          {favoritePets.map((favoritePet) => (
+            <li key={favoritePet._id} className={s.item}>
+              <NoticesItem noticeData={favoritePet} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className={s.text}>
+          <p>
+            Oops, <span className={clsx('brand-color', s.brandColor)}>looks like there aren't any furries</span> on your
+            adorable page yet. Do not worry! View your pets on the "Find pet" page and add them to your favorites.
+          </p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default MyNoticesFavoritePets;
--- src/components/MyNoticesViewedPets/MyNoticesViewedPets.tsx
+++ src/components/MyNoticesViewedPets/MyNoticesViewedPets.tsx
@@ -1,9 +1,20 @@
+import clsx from 'clsx';
 import s from './MyNoticesViewedPets.module.scss';
 
 export interface MyNoticesViewedPetsProps {}
 
 const MyNoticesViewedPets = ({}: MyNoticesViewedPetsProps) => {
-  return <div>MyNoticesViewedPets</div>;
+  return (
+    <div className={s.ьyNoticesViewedPets}>
+      <div className={s.text}>
+        <p>
+          Oops, <span className={clsx('brand-color', s.brandColor)}>looks like there aren't any furries</span> on your
+          viewed page yet. Do not worry! Start exploring pets on the "Find pet" page — your recently viewed pets will
+          appear here.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default MyNoticesViewedPets;
